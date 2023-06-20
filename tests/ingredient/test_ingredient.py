a7524c95a0460b350402e314c8a37535e7e0edd6
Layane Nunes <layanenu@gmail.com>
2023-06-20 12:16:07 -0300

    feat: test_ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,26 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (Ingredient, Restriction)  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient_one = Ingredient("queijo provolone")
+    ingredient_two = Ingredient("tomate")
+    ingrediente_three = Ingredient("queijo provolone")
+
+    assert ingredient_one.name == "queijo provolone"
+    assert ingredient_two.name == "tomate"
+
+    assert ingredient_one.__repr__() == "Ingredient('queijo provolone')"
+    assert ingredient_two.__repr__() == "Ingredient('tomate')"
+
+    assert ingredient_one == ingrediente_three
+    assert ingredient_one != ingredient_two
+
+    assert hash(ingredient_one) == hash("queijo provolone")
+    assert hash(ingredient_two) == hash("tomate")
+    assert hash(ingredient_one) != hash(ingredient_two)
+
+    assert ingredient_one.restrictions == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED,
+    }
