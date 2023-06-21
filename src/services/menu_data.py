fe0fd292d96ea1e69377ade072cc602d282b7257
Layane Nunes <layanenu@gmail.com>
2023-06-20 21:44:57 -0300

    feat: menu_data

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,26 @@
+from models.ingredient import Ingredient
+from models.dish import Dish
+import pandas as pd
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+        self.load_menu_from_csv(source_path)
+
+    def load_menu_from_csv(self, source_path: str):
+        csv_data = pd.read_csv(source_path)
+        dishes = {}
+
+        for row in csv_data.itertuples(index=False):
+            name, price, ingredient_name, amount = row
+
+            dish = dishes.get(name)
+            if not dish:
+                dish = Dish(name, price)
+                dishes[name] = dish
+                self.dishes.add(dish)
+
+            ingredient = Ingredient(ingredient_name)
+            dish.add_ingredient_dependency(ingredient, amount)
