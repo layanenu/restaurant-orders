75c357483a24aacedc2240e2614f576557b710d2
Layane Nunes <layanenu@gmail.com>
2023-06-20 22:26:16 -0300

    feat: get_main_menu

--- src/services/menu_builder.py
+++ src/services/menu_builder.py
@@ -26,4 +26,20 @@ class MenuBuilder:
 
     # Req 4
     def get_main_menu(self, restriction=None) -> pd.DataFrame:
-        pass
+        menu = []
+
+        for dish in self.menu_data.dishes:
+            if (
+                (restriction is None or restriction not in dish
+                 .get_restrictions())
+                and all(ingredient in self.inventory
+                        .inventory for ingredient in dish.get_ingredients())
+            ):
+                menu.append({
+                    "dish_name": dish.name,
+                    "ingredients": dish.get_ingredients(),
+                    "price": dish.price,
+                    "restrictions": dish.get_restrictions()
+                })
+
+        return pd.DataFrame(menu)
